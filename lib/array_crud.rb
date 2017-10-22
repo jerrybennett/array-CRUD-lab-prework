acbd18954cd606babc8411aeb23fbd9113a68618
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-22 16:00:16 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["cat", "dog", "rabbit"]
-  array << "snail"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
